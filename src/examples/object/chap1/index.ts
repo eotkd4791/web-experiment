1e30cd0ac857da1f3926faec76ef11fde4515acd
eotkd4791 <eotkd4791@gmail.com>
2024-11-03 16:29:00 +0900

    feat: 오브젝트 챕터1 예시코드

---
+++ src/examples/object/chap1/index.ts
@@ -0,0 +1,93 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+class Invitation {
+  private when: Date | null = null;
+}
+
+class Ticket {
+  private fee: number = 0;
+
+  public getFee() {
+    return this.fee;
+  }
+}
+
+class Bag {
+  private ticket: Ticket | null = null;
+
+  constructor(private invitation: Invitation | null, private amount: number) {
+    this.invitation = invitation;
+    this.amount = amount;
+  }
+
+  public hasInvitation() {
+    return this.invitation !== null;
+  }
+
+  public hasTicket() {
+    return this.ticket !== null;
+  }
+
+  public setTicket(ticket: Ticket | null) {
+    this.ticket = ticket;
+  }
+
+  public minusAmount(amount: number) {
+    this.amount -= amount;
+  }
+
+  public plusAmount(amount: number) {
+    this.amount += amount;
+  }
+}
+
+class Audience {
+  constructor(private bag: Bag | null) {}
+
+  public getBag() {
+    return this.bag;
+  }
+}
+
+class TicketOffice {
+  constructor(private amount: number, private tickets: Ticket[]) {}
+
+  public getTicket() {
+    return this.tickets.shift() || null;
+  }
+
+  public minusAmount(amount: number) {
+    this.amount -= amount;
+  }
+
+  public plusAmount(amount: number) {
+    this.amount += amount;
+  }
+}
+
+class TickerSeller {
+  constructor(private ticketOffice: TicketOffice) {}
+
+  public getTicketOffice() {
+    return this.ticketOffice;
+  }
+}
+
+class Theater {
+  constructor(private ticketSeller: TickerSeller) {}
+
+  public enter(audidence: Audience) {
+    if (audidence.getBag()?.hasInvitation()) {
+      const ticket = this.ticketSeller.getTicketOffice().getTicket();
+
+      audidence.getBag()?.setTicket(ticket);
+    } else {
+      const ticket = this.ticketSeller.getTicketOffice().getTicket();
+      if (ticket) {
+        audidence.getBag()?.minusAmount(ticket.getFee());
+        this.ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+        audidence.getBag()?.setTicket(ticket);
+      }
+    }
+  }
+}
