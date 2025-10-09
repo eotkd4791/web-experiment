12f252ffd17bea681024039a4ecafcf7ac5527f3
eotkd4791 <eotkd4791@gmail.com>
2025-10-09 17:31:40 +0900

    chore: setup shadcn

--- packages/react/src/main.tsx
+++ packages/react/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
 // async function enableMocking() {
 //   const { worker } = await import("./mocks/browser");
--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,12 +1,13 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
