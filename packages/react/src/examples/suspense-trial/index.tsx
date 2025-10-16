d88f337723228d07e7c7e28906ce5c43954b96a5
eotkd4791 <eotkd4791@gmail.com>
2025-10-17 01:20:27 +0900

    feat: startTransition 적용 차이

--- packages/react/src/examples/suspense-trial/index.tsx
+++ packages/react/src/examples/suspense-trial/index.tsx
@@ -1,12 +1,15 @@
-import { Suspense } from "react";
+import { Suspense, useState, startTransition } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { User } from "./User";
 
 export function SuspenseTrial() {
+  const [userId, setUserId] = useState(1);
+
   return (
     <ErrorBoundary fallback={<div>error boundary</div>}>
       <Suspense fallback={<div>suspense fallback</div>}>
-        <User id={2} />
+        <button onClick={() => startTransition(() => setUserId((prev) => prev + 1))}>next</button>
+        <User id={userId} />
       </Suspense>
     </ErrorBoundary>
   );
