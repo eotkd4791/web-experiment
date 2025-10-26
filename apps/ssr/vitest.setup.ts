be9ab5220d9722f0a79b2e2665d50dae506bfd20
eotkd4791 <eotkd4791@gmail.com>
2025-10-26 14:05:37 +0900

    test: add first test

--- apps/ssr/vitest.setup.ts
+++ apps/ssr/vitest.setup.ts
@@ -1,6 +1,13 @@
 /// <reference types="vitest" />
 
 import "@testing-library/jest-dom";
+import { server } from "./test/mocks/server";
+
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
 
 vi.mock("next/navigation", () => {
   return {
