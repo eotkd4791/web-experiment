52fda930dae41b04b3cd11e711bb0ff4160ef1d7
eotkd4791 <eotkd4791@gmail.com>
2026-08-14 09:05:49 +0900

    chore: adjust oxlint, oxfmt

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,6 @@
 import js from '@eslint/js'
+import tanstackQuery from '@tanstack/eslint-plugin-query'
+import oxlint from 'eslint-plugin-oxlint'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -7,11 +9,7 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      'plugin:@tanstack/eslint-plugin-query/recommended',
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -22,8 +20,11 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
+  ...tanstackQuery.configs['flat/recommended'],
+  ...oxlint.buildFromOxlintConfigFile(new URL('./.oxlintrc.json', import.meta.url).pathname),
 )
